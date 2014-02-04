b870f4c77d8581d2f55a0172981c78a1857c637c
Darron Froese <darron@froese.org>
2014-02-04 16:25:09 -0700

    First commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+name             'denyhosts'
+maintainer       'Darron Froese'
+maintainer_email 'darron@froese.org'
+license          'Apache 2.0'
+description      'Installs/configures something'
+version          '0.1.0'
+recipe           'denyhosts::default', 'Installs/configures something'
